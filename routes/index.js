184869e81780e0886121dfa37214ca1d12b8f4c9
D'Arcey Taylor <darceytaylor05@gmail.com>
2016-02-04 15:54:03 -0500

    navbar commit

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express on Azure' });
 });
+//Get About page
+router.get('/About', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+//Get Contact page
+router.get('/Contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
 
 module.exports = router;
